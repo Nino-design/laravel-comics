be3751f8f99856960397bb4925b5dcb7c918cf39
Nino-design <npompili@gmail.com>
2022-06-26 11:18:56 +0200

    header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,50 @@
-<h1>header</h1>
+<header class="header">
+    <div class="header-top">
+        <div class="container d-flex justify-content-end align-items-center">
+            <ul class="d-flex align-items-center">
+                <li><a href="">DC power visa</a></li>
+                <li><a href="">additional DC sites</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="header-wrapper container">
+        <div class="header-img">
+            <img src="{{ asset('images/dc-logo.png') }}" />
+        </div>
+        <div>
+            <ul class="header-list">
+                <li>
+                    <a href="">CHARACTERS</a>
+                </li>
+                <li>
+                    <a href="">COMICS</a>
+                </li>
+                <li>
+                    <a href="">MOVIES</a>
+                </li>
+                <li>
+                    <a href="">TV</a>
+                </li>
+                <li>
+                    <a href="">COLLECTIBLES</a>
+                </li>
+                <li>
+                    <a href="">VIDEOS</a>
+                </li>
+                <li>
+                    <a href="">FANS</a>
+                </li>
+                <li>
+                    <a href="">NEWS</a>
+                </li>
+                <li>
+                    <a href="">Characters</a>
+                </li>
+                <li>
+                    <a href="">Characters</a>
+                </li>
+            </ul>
+        </div>
+        <input type="text" placeholder="Search">
+    </div>
+</header>
